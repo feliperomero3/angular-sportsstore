8de49bbcf22f0073a908349535610704fc32b302
Felipe Romero <feliperomero3@outlook.com>
2021-02-19 22:36:36 -0600

    Update the Root Component

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-sportsstore app is running!');
+    expect(page.getTitleText()).toEqual('This is angular-sportsstore');
   });
 
   afterEach(async () => {
