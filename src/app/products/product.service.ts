a6aa33a0c1081749993cdf1581e3788df3510d7c
Felipe Romero <feliperomero3@outlook.com>
2021-02-20 21:37:41 -0600

    Create the Feature Module

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,28 +1,25 @@
 import { Injectable } from '@angular/core';
 import { from, Observable } from 'rxjs';
 import { Product } from './product.model';
+import { products } from './products';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class ProductService {
-  private products: Product[] = [
-    new Product(1, 'Product 1', 'Category 1', 'Product 1 (Category 1)', 100),
-    new Product(2, 'Product 2', 'Category 1', 'Product 2 (Category 1)', 100),
-    new Product(3, 'Product 3', 'Category 1', 'Product 3 (Category 1)', 100),
-    new Product(4, 'Product 4', 'Category 1', 'Product 4 (Category 1)', 100),
-    new Product(5, 'Product 5', 'Category 1', 'Product 5 (Category 1)', 100),
-    new Product(6, 'Product 6', 'Category 2', 'Product 6 (Category 2)', 100),
-    new Product(7, 'Product 7', 'Category 2', 'Product 7 (Category 2)', 100),
-    new Product(8, 'Product 8', 'Category 2', 'Product 8 (Category 2)', 100),
-    new Product(9, 'Product 9', 'Category 2', 'Product 9 (Category 2)', 100),
-    new Product(10, 'Product 10', 'Category 2', 'Product 10 (Category 2)', 100),
-    new Product(11, 'Product 11', 'Category 3', 'Product 11 (Category 3)', 100),
-    new Product(12, 'Product 12', 'Category 3', 'Product 12 (Category 3)', 100),
-    new Product(13, 'Product 13', 'Category 3', 'Product 13 (Category 3)', 100),
-    new Product(14, 'Product 14', 'Category 3', 'Product 14 (Category 3)', 100),
-    new Product(15, 'Product 15', 'Category 3', 'Product 15 (Category 3)', 100)
-  ];
+  private products: Product[] = products;
 
-  getProducts(): Observable<Product[]> {
-    return from([this.products]);
+  getProducts(category: string = null): Observable<Product[]> {
+    return from([this.products.filter(p => category == null || p.category == category)]);
   }
+
+  getProduct(id: number): Observable<Product> {
+    return from([this.products.find(p => p.id == id)]);
+  }
+
+  getCategories(): Observable<string[]> {
+    return from([this.products.map(p => p.category)
+      .filter((c, index, array) => array.indexOf(c) == index).sort()]);
+  }
+
 }
