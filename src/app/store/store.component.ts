7d403dcbbb05d8bc3b4ad8d67fe55155b56b10a4
Felipe Romero <feliperomero3@outlook.com>
2021-02-23 19:37:56 -0600

    Remove empty Component's CSS style sheets
    Fix $6

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -6,7 +6,7 @@ import { ProductService } from '../products/product.service';
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css']
+  styles: []
 })
 export class StoreComponent implements OnInit {
   products: Product[];
