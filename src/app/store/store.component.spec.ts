2595cc5998f4bcce0ac3d60fa4a25187f8130b2e
Felipe Romero <feliperomero3@outlook.com>
2021-02-23 19:07:55 -0600

    Create cart summary stub component to be used in shallow unit tests

--- src/app/store/store.component.spec.ts
+++ src/app/store/store.component.spec.ts
@@ -1,14 +1,22 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CounterDirective } from './counter.directive';
 import { StoreComponent } from './store.component';
 
 describe('StoreComponent', () => {
   let component: StoreComponent;
   let fixture: ComponentFixture<StoreComponent>;
 
+  @Component({ selector: 'app-cart-summary', template: '' })
+  class CartSummaryStubComponent {
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        StoreComponent
+        StoreComponent,
+        CounterDirective,
+        CartSummaryStubComponent
       ]
     }).compileComponents();
   }));
