6c4edd4693ab3365d486b9b4abb6411087d30bb2
Felipe Romero <feliperomero3@outlook.com>
2021-03-10 00:01:42 -0600

    Extend the Order Service

--- src/app/orders/order.service.ts
+++ src/app/orders/order.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { AuthService } from '../auth/auth.service';
 import { Order } from './order.model';
 
 const PROTOCOL = environment.protocol;
@@ -12,22 +13,31 @@ const HOST = environment.host;
   providedIn: 'root'
 })
 export class OrderService {
-  private orders: Order[];
   private baseUrl = `${PROTOCOL}://${HOST}:${PORT}/`;
   private httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      Authorization: `Bearer<${this.auth.Token}>`
     })
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
 
-  getOrders(): Order[] {
-    return this.orders;
+  getOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.baseUrl + 'orders', this.httpOptions);
   }
 
   saveOrder(order: Order): Observable<Order> {
     console.log(JSON.stringify(order));
     return this.http.post<Order>(this.baseUrl + 'orders', order, this.httpOptions);
   }
+
+  deleteOrder(id: number): Observable<Order> {
+    return this.http.delete<Order>(this.baseUrl + `orders/${id}`, this.httpOptions);
+  }
+
+  updateOrder(order: Order): Observable<Order> {
+    return this.http.put<Order>(this.baseUrl + `orders/${order.id}`, order, this.httpOptions);
+  }
+
 }
