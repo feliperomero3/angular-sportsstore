0a2054457b6cd6356565c8cc0639e4d8a58b5e4b
Felipe Romero <feliperomero3@outlook.com>
2021-03-13 21:14:12 -0600

    Implement the Product Editor Component.

--- src/app/admin/product-editor.component.spec.ts
+++ src/app/admin/product-editor.component.spec.ts
@@ -1,13 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../products/product.service';
 import { ProductEditorComponent } from './product-editor.component';
 
 describe('ProductEditorComponent', () => {
   let component: ProductEditorComponent;
   let fixture: ComponentFixture<ProductEditorComponent>;
+  let mockProductService: any;
 
   beforeEach(async(() => {
+    mockProductService = jasmine.createSpyObj('ProductService', ['getProducts', 'deleteProduct']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    const activatedRouteSpy = {
+      snapshot: {
+        params: {
+          mode: 'create'
+        }
+      }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ProductEditorComponent]
+      declarations: [ProductEditorComponent],
+      imports: [FormsModule],
+      providers: [
+        { provide: ProductService, useValue: mockProductService },
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteSpy }
+      ]
     }).compileComponents();
   }));
 
--- src/app/admin/product-editor.component.ts
+++ src/app/admin/product-editor.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../products/product.model';
+import { ProductService } from '../products/product.service';
 
 @Component({
-  template: `
-    <div class="bg-warning p-2 text-white">
-      <h3>Product Editor Placeholder</h3>
-    </div>
-  `
+  templateUrl: 'product-editor.component.html'
 })
-export class ProductEditorComponent implements OnInit {
+export class ProductEditorComponent {
+  isEditing = false;
+  product: Product = new Product();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    activeRoute: ActivatedRoute) {
+    this.isEditing = activeRoute.snapshot.params.mode === 'edit';
+    if (this.isEditing) {
+      Object.assign(this.product,
+        productService.getProduct(+activeRoute.snapshot.params.id));
+    }
   }
 
+  save(form: NgForm): void {
+    this.productService.saveProduct(this.product).subscribe({
+      next: p => console.error('Product ' + p.id + 'saved.')
+    });
+    this.router.navigateByUrl('/admin/main/products');
+  }
 }
