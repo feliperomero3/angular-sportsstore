703609823d546e60768df295b44de80d153cddc8
Felipe Romero <feliperomero3@outlook.com>
2021-03-13 20:29:11 -0600

    Implement the Product Table Component.

--- src/app/admin/product-table.component.spec.ts
+++ src/app/admin/product-table.component.spec.ts
@@ -1,19 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { ProductService } from '../products/product.service';
 import { ProductTableComponent } from './product-table.component';
 
 describe('ProductTableComponent', () => {
   let component: ProductTableComponent;
   let fixture: ComponentFixture<ProductTableComponent>;
+  let mockProductService: any;
 
   beforeEach(async(() => {
+    mockProductService = jasmine.createSpyObj('ProductService', ['getProducts', 'deleteProduct']);
+
     TestBed.configureTestingModule({
-      declarations: [ProductTableComponent]
+      declarations: [ProductTableComponent],
+      providers: [{ provide: ProductService, useValue: mockProductService }]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductTableComponent);
     component = fixture.componentInstance;
+    mockProductService.getProducts.and.returnValue(of([]));
     fixture.detectChanges();
   });
 
--- src/app/admin/product-table.component.ts
+++ src/app/admin/product-table.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Product } from '../products/product.model';
+import { ProductService } from '../products/product.service';
 
 @Component({
   selector: 'app-product-table',
-  template: `
-    <div class="bg-info p-2 text-white">
-      <h3>Product Table Placeholder</h3>
-    </div>
-  `
+  templateUrl: 'product-table.component.html'
 })
-export class ProductTableComponent implements OnInit {
+export class ProductTableComponent {
+  private products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
+  getProducts(): Product[] {
+    this.productService.getProducts().subscribe({
+      next: data => this.products = data
+    });
+    return this.products;
   }
 
+  deleteProduct(id: number): void {
+    this.productService.deleteProduct(id).subscribe({
+      next: (product) => console.error('Deleted Product: ' + product)
+    });
+  }
 }
