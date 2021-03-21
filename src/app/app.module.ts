b53eb9a7c8701a69756c95aec4333bb9d2cca5f9
Felipe Romero <feliperomero3@outlook.com>
2021-03-21 00:33:08 -0600

    Remove superfluous imported modules on app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
-import { ProductModule } from './products/product.module';
-import { CartModule } from './cart/cart.module';
-import { OrdersModule } from './orders/orders.module';
 import { AdminModule } from './admin/admin.module';
 
 @NgModule({
@@ -17,9 +13,6 @@ import { AdminModule } from './admin/admin.module';
     BrowserModule,
     AppRoutingModule,
     StoreModule,
-    CartModule,
-    OrdersModule,
-    ProductModule,
     AdminModule
   ],
   providers: [],
